55d993cb2d43776c584644286144a123ae987267
hotgirl6969 <71122189+hotgirl6969@users.noreply.github.com>
2021-03-28 22:36:47 +0200

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -11,7 +11,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#ff8fcd";
+static const char col_cyan[]        = "#ffb5d5";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
